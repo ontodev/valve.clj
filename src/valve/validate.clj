bb2a706a9d2b43cf31fea0930ea3d4f59ca14d24
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-08-08 10:32:15 -0400

    finish implementing first basic unit test

--- src/valve/validate.clj
+++ src/valve/validate.clj
@@ -1,5 +1,7 @@
 (ns valve.validate
-  (:require [valve.log :as log]))
+  (:require ;; TODO: pprint is used for debugging during dev. Remove this dependency later.
+            [clojure.pprint :refer [pprint]]
+            [valve.log :as log]))
 
 ;; Builtin validate functions
 (defn validate-any
@@ -84,100 +86,74 @@
                                  "check" ["field" "string" "named:direct?"]
                                  "validate" validate-under}})
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Custom functions used only for dev.
-;; TODO: Remove this section later.
-(defn validate-custom-1
-  [config args table column row-idx value]
-  {:abba "gabba"})
-
-(defn validate-custom-2
-  [config args table column row-idx value]
-  {:abba "gabba"})
-
-(def custom-functions
-  {"custom-1" {"usage" "any(expression+)"
-               "check" ["expression+"]
-               "validate" validate-custom-1}
-   "custom-2" {"usage" "any(expression+)"
-               "check" ["expression+"]
-               "validate" validate-custom-2}})
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(defn check-custom-function
-  "TODO: Add docstring here"
-  [[func-name details]]
-  (when (contains? default-functions func-name)
-    (throw (Exception. (str "Custom function: " func-name " duplicates a builtin function name"))))
-
-  (when-not (contains? details "validate")
-    (throw (Exception. (str "Details of " func-name " must include a 'validate' key"))))
-
-  (let [full-name (str "valve.validate/validate-" func-name)
-        ;; See https://stackoverflow.com/a/1748508/8599709
-        params (->> full-name
-                    (symbol)
-                    (ns-resolve *ns*)
-                    (meta)
-                    :arglists
-                    (#(if (> (count %) 1)
-                        (throw
-                         (Exception.
-                          (str "Custom function: " full-name " has "
-                               "more than one argument list")))
-                        (first %)))
-                    (map name))]
-    (when-not (= (nth params 0) "config")
-      (throw (Exception. (str "'" full-name "' argument 1 must be 'config'"))))
-    (when-not (= (nth params 1) "args")
-      (throw (Exception. (str "'" full-name "' argument 2 must be 'args'"))))
-    (when-not (= (nth params 2) "table")
-      (throw (Exception. (str "'" full-name "' argument 3 must be 'table'"))))
-    (when-not (= (nth params 3) "column")
-      (throw (Exception. (str "'" full-name "' argument 4 must be 'column'"))))
-    (when-not (= (nth params 4) "row-idx")
-      (throw (Exception. (str "'" full-name "' argument 5 must be 'row-idx'"))))
-    (when-not (= (nth params 5) "value")
-      (throw (Exception. (str "'" full-name "' argument 6 must be 'value'")))))
-
-  ;; At the end, return the vector back unchanged
-  [func-name details])
-
 (defn validate
   "TODO: Insert docstring here"
-  ;; TODO: Remove this comment later:
-  ;; To test `validate` directly in the repl, send a command like the following:
-  ;; (handle-cli-opts ["-o" "./output.csv" "-d" "./distinct/" "./input1.csv"])
-  ([paths add-functions distinct-messages row-start]
-   (when (and add-functions
-              (not (instance? java.util.Map add-functions)))
-     (throw (Exception. "Value for add_functions must be a map")))
+  ([paths custom-functions custom-namespace distinct-messages row-start]
+   (when (and custom-functions
+              (not (instance? java.util.Map custom-functions)))
+     (throw (Exception. "Value for custom-functions must be a map")))
 
-   (let [;; TODO: remove this override of add-functions later:
-         add-functions custom-functions
+   (letfn [(check-custom [[func-name details]]
+             (when (contains? default-functions func-name)
+               (throw (Exception.
+                       (str "Custom function: " func-name " duplicates a builtin function name"))))
 
-         functions (->> add-functions
-                        (seq)
-                        (map check-custom-function)
-                        (apply concat)
-                        (apply hash-map)
-                        (merge default-functions))
+             (when-not (contains? details "validate")
+               (throw (Exception. (str "Details of " func-name " must include a 'validate' key"))))
 
-         ;; TODO: Check for directories and list their entire contents
-         fixed-paths (do)
-         ;; TODO: Load all tables, error on duplicates
-         table-details (do)
-         config (do)
-         ;; TODO: Load datatype, field, and rule - stop process on any problems
-         setup-messages (do)
-         kill-messages (do)
-         ;; TODO: Run validation
-         messages (do)]
+             (let [qualified-name (str custom-namespace "/validate-" func-name)
+                   ;; See https://stackoverflow.com/a/1748508/8599709
+                   params (->> qualified-name
+                               (symbol)
+                               (ns-resolve *ns*)
+                               (meta)
+                               :arglists
+                               (#(if (> (count %) 1)
+                                   (throw
+                                    (Exception.
+                                     (str "Custom function: " qualified-name " has "
+                                          "more than one argument list")))
+                                   (first %)))
+                               (map name))]
+               (when-not (= (nth params 0) "config")
+                 (throw (Exception. (str "'" qualified-name "' argument 1 must be 'config'"))))
+               (when-not (= (nth params 1) "args")
+                 (throw (Exception. (str "'" qualified-name "' argument 2 must be 'args'"))))
+               (when-not (= (nth params 2) "table")
+                 (throw (Exception. (str "'" qualified-name "' argument 3 must be 'table'"))))
+               (when-not (= (nth params 3) "column")
+                 (throw (Exception. (str "'" qualified-name "' argument 4 must be 'column'"))))
+               (when-not (= (nth params 4) "row-idx")
+                 (throw (Exception. (str "'" qualified-name "' argument 5 must be 'row-idx'"))))
+               (when-not (= (nth params 5) "value")
+                 (throw (Exception. (str "'" qualified-name "' argument 6 must be 'value'"))))
 
-     ;; TODO: Return messages, logging an error if the list is not empty.
-     (or messages [])))
+               ;; After validating the given custom function's details, return them unchanged:
+               [func-name details]))]
+
+     (let [;; Register functions:
+           functions (->> custom-functions
+                          (seq)
+                          (map check-custom)
+                          (apply concat)
+                          (apply hash-map)
+                          (merge default-functions))
+
+           ;; TODO: Check for directories and list their entire contents
+           fixed-paths (do)
+           ;; TODO: Load all tables, error on duplicates
+           table-details (do)
+           config (do)
+           ;; TODO: Load datatype, field, and rule - stop process on any problems
+           setup-messages (do)
+           kill-messages (do)
+           ;; TODO: Run validation
+           messages (do)]
+
+       (clojure.pprint/pprint functions)
+
+       ;; TODO: Return messages, logging an error if the list is not empty.
+       (or messages []))))
 
   ([paths distinct-messages row-start]
-   (validate paths nil distinct-messages row-start)))
+   (validate paths {} nil distinct-messages row-start)))
--- test/valve/core_test.clj
+++ test/valve/core_test.clj
@@ -1,7 +1,28 @@
 (ns valve.core-test
   (:require [clojure.test :refer :all]
-            [valve.cli-handler :refer [handle-cli-opts]]))
+            [valve.cli-handler :refer [handle-cli-opts]]
+            [valve.validate :refer [validate]]))
 
-(deftest end-to-end
-  (testing "Ent-to-ent test"
+(defn- validate-custom-1
+  [config args table column row-idx value]
+  {:abba "gabba"})
+
+(defn- validate-custom-2
+  [config args table column row-idx value]
+  {:abba "gabba"})
+
+(def ^:private custom-functions
+  {"custom-1" {"usage" "any(expression+)"
+               "check" ["expression+"]
+               "validate" validate-custom-1}
+   "custom-2" {"usage" "any(expression+)"
+               "check" ["expression+"]
+               "validate" validate-custom-2}})
+
+(deftest test-validate
+  (testing "Validate test"
+    (is (= [] (validate ["output.csv"] custom-functions "valve.core-test" "distinct" 2)))))
+
+(deftest test-end-to-end
+  (testing "End-to-end test"
     (is (= 0 (handle-cli-opts ["-o" "./output.csv" "-d" "./distinct/" "./input1.csv"])))))
